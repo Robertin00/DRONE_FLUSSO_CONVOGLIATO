ab85fb88ba91a4d33e3b3887d044f36ed482553d
Robertin00 <iphone13robert@gmail.com>
2025-03-25 13:39:48 +0100

    Add files via upload

---
+++ MPU6050 LIBRERIA DI GESTIONE/Src/MPU6050_platform.c
@@ -0,0 +1,12 @@
+/**
+ * @file MPU6050_platform.c
+ * @author Robert Laurentiu Mincu
+ * @brief Il file contiene le definizioni delle funzioni create appositamente per poter 
+ *        interfacciare il sensore a NUCLEO-H745ZI-Q utilizzando STCubeIDE
+ * @version 0.1
+ * @date 25/03/2025
+ */
+
+#include "MPU6050_platform.h"
+#include "stdio.h"
+#include <stdio.h>
